ae2a47b5fb03a722ed1630b506d362ec76c58b6f
Karthick <karthyuom@gmail.com>
2014-09-20 17:24:54 +0800

    Test cases changed

--- cachematrix.R
+++ cachematrix.R
@@ -72,7 +72,7 @@ cacheSolve <- function(make.matrix, ...) {
 ##[2,]    1 -0.5
 
 ## >  cacheSolve(amatrix)   # Returns cached matrix inverse using previously computed matrix inverse
-##getting cached data...
+##Getting cached data...
 ##     [,1] [,2]
 ##[1,]   -2  1.5
 ##[2,]    1 -0.5
